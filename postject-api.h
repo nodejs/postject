e55377a3919be8d8c7e3bd7bc86d484b50595e95
David Sanders <david.sanders@postman.com>
2022-06-09 14:25:25 -0700

    chore: improvements to C API

--- postject-api.h
+++ postject-api.h
@@ -1,6 +1,9 @@
 // Copyright (c) 2022 Postman, Inc.
 
-#include <cstddef>
+#ifndef POSTJECT_API_H_
+#define POSTJECT_API_H_
+
+#include <stddef.h>
 
 #if defined(__APPLE__) && defined(__MACH__)
 #include <mach-o/getsect.h>
@@ -10,22 +13,31 @@
 #include <libloaderapi.h>
 #endif
 
-static constexpr const char* MACHO_DEFAULT_SEGMENT_NAME = "__POSTJECT";
-
 struct MachOOptions {
-  const char* framework_name = nullptr;
-  const char* segment_name = MACHO_DEFAULT_SEGMENT_NAME;
+  const char* framework_name;
+  const char* segment_name;
 };
 
+void postject_macho_options_init(MachOOptions* options) {
+  options->framework_name = nullptr;
+  options->segment_name = nullptr;
+}
+
 // TODO
-void* postject_find_resource(const char* name, size_t* size, MachOOptions* macho_options = nullptr) {
+void* postject_find_resource(const char* name, size_t* size, const MachOOptions* macho_options = nullptr) {
 #if defined(__APPLE__) && defined(__MACH__)
   unsigned long section_size;
   char* ptr = nullptr;
   if (macho_options && macho_options->framework_name != nullptr) {
     ptr = getsectdatafromFramework(macho_options->framework_name, macho_options->segment_name, name, &section_size);
   } else {
-    ptr = getsectdata(macho_options->segment_name, name, &section_size);
+    const char* segment_name = "__POSTJECT";
+
+    if (macho_options && macho_options->segment_name != nullptr) {
+      segment_name = macho_options->segment_name;
+    }
+
+    ptr = getsectdata(segment_name, name, &section_size);
   }
   *size = static_cast<size_t>(section_size);
 
@@ -34,3 +46,5 @@ void* postject_find_resource(const char* name, size_t* size, MachOOptions* macho
 
   return nullptr;
 }
+
+#endif  // POSTJECT_API_H_
