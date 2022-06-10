eeda474b501e9887f952027684fe835033b3e1fe
Darshan Sen <darshan.sen@postman.com>
2022-06-10 14:19:17 +0000

    Merged in chore/turn-postject-api.h-into-a-C-API (pull request #1)
    chore: turn `postject-api.h` into a C API
    
    * chore: turn postject-api.h into a C API
    
    This guarantees ABI compatibility across multiple compiler versions.
    
    Signed-off-by: Darshan Sen <raisinten@gmail.com>
    
    
    Approved-by: David Sanders

--- postject-api.h
+++ postject-api.h
@@ -19,42 +19,42 @@ struct MachOOptions {
   const char* segment_name;
 };
 
-void postject_macho_options_init(MachOOptions* options) {
-  options->framework_name = nullptr;
-  options->segment_name = nullptr;
+void postject_macho_options_init(struct MachOOptions* options) {
+  options->framework_name = NULL;
+  options->segment_name = NULL;
 }
 
-void* postject_find_resource(const char* name, size_t* size, const MachOOptions* macho_options = nullptr) {
+void* postject_find_resource(const char* name, size_t* size, const struct MachOOptions* macho_options) {
 #if defined(__APPLE__) && defined(__MACH__)
   unsigned long section_size;
-  char* ptr = nullptr;
-  if (macho_options && macho_options->framework_name != nullptr) {
+  char* ptr = NULL;
+  if (macho_options && macho_options->framework_name != NULL) {
     ptr = getsectdatafromFramework(macho_options->framework_name, macho_options->segment_name, name, &section_size);
   } else {
     const char* segment_name = "__POSTJECT";
 
-    if (macho_options && macho_options->segment_name != nullptr) {
+    if (macho_options && macho_options->segment_name != NULL) {
       segment_name = macho_options->segment_name;
     }
 
     ptr = getsectdata(segment_name, name, &section_size);
   }
-  *size = static_cast<size_t>(section_size);
+  *size = (size_t) section_size;
 
   return ptr;
 #elif defined(__linux__)
   // TODO - Implement for ELF
 #elif defined(_WIN32)
-  HRSRC resource_handle = FindResourceA(nullptr, resource_name, RT_RCDATA);
+  HRSRC resource_handle = FindResourceA(NULL, resource_name, RT_RCDATA);
 
-  if (resource_handle != nullptr) {
-    *size = SizeofResource(nullptr, resource_handle);
-    return LockResource(LoadResource(nullptr, resource_handle));
+  if (resource_handle != NULL) {
+    *size = SizeofResource(NULL, resource_handle);
+    return LockResource(LoadResource(NULL, resource_handle));
   }
 #endif
 
   *size = 0;
-  return nullptr;
+  return NULL;
 }
 
 #endif  // POSTJECT_API_H_
