3a3e28c63e52fd26837f1ffe40984f49a362f332
David Sanders <david.sanders@postman.com>
2022-09-09 20:16:06 -0700

    wip: initial porting of PE code

--- src/postject.cpp
+++ src/postject.cpp
@@ -1,3 +1,6 @@
+#include <algorithm>
+#include <codecvt>
+#include <locale>
 #include <memory>
 #include <vector>
 
@@ -155,8 +158,117 @@ emscripten::val inject_into_pe(const emscripten::val& executable,
   std::unique_ptr<LIEF::PE::Binary> binary =
       LIEF::PE::Parser::parse(vec_from_val(executable));
 
-  // TODO
+  if (!binary) {
+    object.set("result", emscripten::val(InjectResult::kError));
+    return object;
+  }
 
+  // TODO - lief.PE.ResourcesManager doesn't support RCDATA it seems, add
+  // support so this is simpler?
+
+  if (!binary->has_resources()) {
+    // TODO - Handle this edge case by creating the resource tree
+    object.set("result", emscripten::val(InjectResult::kError));
+    return object;
+  }
+
+  LIEF::PE::ResourceNode* resources = binary->resources();
+
+  LIEF::PE::ResourceNode* rcdata_node = nullptr;
+  LIEF::PE::ResourceNode* id_node = nullptr;
+
+  // First level => Type (ResourceDirectory node)
+  auto rcdata_node_iter = std::find_if(
+      std::begin(resources->childs()), std::end(resources->childs()),
+      [](const LIEF::PE::ResourceNode& node) {
+        return node.id() ==
+               static_cast<uint32_t>(LIEF::PE::ResourceNode::TYPE::DATA);
+      });
+
+  if (rcdata_node_iter != std::end(resources->childs())) {
+    rcdata_node = &*rcdata_node_iter;
+  } else {
+    LIEF::PE::ResourceDirectory new_rcdata_node;
+    new_rcdata_node.id(
+        static_cast<uint32_t>(LIEF::PE::ResourceNode::TYPE::DATA));
+    rcdata_node = &resources->add_child(new_rcdata_node);
+  }
+
+  // Second level => ID (ResourceDirectory node)
+  auto id_node_iter = std::find_if(
+      std::begin(rcdata_node->childs()), std::end(rcdata_node->childs()),
+      [resource_name](const LIEF::PE::ResourceNode& node) {
+        return node.name() ==
+               std::wstring_convert<std::codecvt_utf8_utf16<char16_t>,
+                                    char16_t>{}
+                   .from_bytes(resource_name);
+      });
+
+  if (id_node_iter != std::end(rcdata_node->childs())) {
+    id_node = &*id_node_iter;
+  } else {
+    LIEF::PE::ResourceDirectory new_id_node;
+    new_id_node.name(resource_name);
+    // TODO - This isn't documented, but if this isn't set then LIEF won't save
+    //        the name. Seems like LIEF should be able to automatically handle
+    //        this if you've set the node's name
+    new_id_node.id(0x80000000);
+    id_node = &rcdata_node->add_child(new_id_node);
+  }
+
+  // Third level => Lang (ResourceData node)
+  if (id_node->childs() != std::end(id_node->childs())) {
+    if (!overwrite) {
+      object.set("result", emscripten::val(InjectResult::kAlreadyExists));
+      return object;
+    }
+
+    id_node->delete_child(*id_node->childs());
+  }
+
+  LIEF::PE::ResourceData lang_node;
+  lang_node.content(vec_from_val(data));
+  id_node->add_child(lang_node);
+
+  binary->remove_section(".rsrc", true);
+
+  // Write out the binary, only modifying the resources
+  LIEF::PE::Builder builder(*binary);
+  builder.build_dos_stub(true);
+  builder.build_imports(false);
+  builder.build_overlay(false);
+  builder.build_relocations(false);
+  builder.build_resources(true);
+  builder.build_tls(false);
+  builder.build();
+
+  // TODO - Why doesn't LIEF just replace the .rsrc section?
+  //        Can we at least change build_resources to take a section name?
+
+  // Re-parse the output so the .l2 section is available
+  binary = LIEF::PE::Parser::parse(builder.get_build());
+
+  // Rename the rebuilt resource section
+  LIEF::PE::Section* section = binary->get_section(".l2");
+  section->name(".rsrc");
+
+  LIEF::PE::Builder builder2(*binary);
+  builder2.build_dos_stub(true);
+  builder2.build_imports(false);
+  builder2.build_overlay(false);
+  builder2.build_relocations(false);
+  builder2.build_resources(false);
+  builder2.build_tls(false);
+  builder2.build();
+
+  // Construct a new Uint8Array in JS
+  const std::vector<uint8_t>& output = builder2.get_build();
+  emscripten::val view{
+      emscripten::typed_memory_view(output.size(), output.data())};
+  auto output_data = emscripten::val::global("Uint8Array").new_(output.size());
+  output_data.call<void>("set", view);
+
+  object.set("data", output_data);
   object.set("result", emscripten::val(InjectResult::kSuccess));
 
   return object;
