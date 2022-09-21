284ef7d7879ff6e13644ec467f503eb461c1531d
Darshan Sen <raisinten@gmail.com>
2022-09-21 12:29:15 +0530

    ci: build and test on linux on CI (#24)
    * ci: build and test on linux on CI
    
    The only Linux distributions we were building and testing on are alpine
    and debian. This change also adds the linux executor which was unused.
    
    Signed-off-by: Darshan Sen <raisinten@gmail.com>
    
    * ci: use sudo while running apt on linux
    
    Signed-off-by: Darshan Sen <raisinten@gmail.com>
    
    * build: use sudo while running apt on linux
    
    Signed-off-by: Darshan Sen <raisinten@gmail.com>
    
    Signed-off-by: Darshan Sen <raisinten@gmail.com>

--- build/deps.mk
+++ build/deps.mk
@@ -4,9 +4,15 @@ EXECUTOR ?= $(OS)
 .PHONY: install-deps
 install-deps:
 # this assumes the linux executor on CircleCI runs ubuntu/debian
-ifneq (,$(filter $(EXECUTOR),debian linux))
+ifneq (,$(filter $(EXECUTOR),debian))
 	apt-get update
 	apt-get install --no-install-recommends -y \
+		build-essential ninja-build cmake \
+		python3 python3-dev python3-setuptools
+endif
+ifneq (,$(filter $(EXECUTOR),linux))
+	sudo apt-get update
+	sudo apt-get install --no-install-recommends -y \
 		build-essential ninja-build cmake \
 		python3 python3-dev python3-setuptools
 endif
