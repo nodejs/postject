2bb8618a318a6b17a8f4b10337c2905f65a85a03
Darshan Sen <raisinten@gmail.com>
2022-10-19 18:44:25 +0530

    fix: compiler warning on Windows
    ```console
    C:\Users\circleci\project\test\test.c(13,5): error C2220: the following warning is treated as an error [C:\Users\circleci\project\build\test\c_test.vcxproj]
    C:\Users\circleci\project\test\test.c(13,5): warning C4996: 'strncpy': This function or variable may be unsafe. Consider using strncpy_s instead. To disable deprecation, use _CRT_SECURE_NO_WARNINGS. See online help for details. [C:\Users\circleci\project\build\test\c_test.vcxproj]
    ```
    
    Refs: https://app.circleci.com/pipelines/github/postmanlabs/postject/187/workflows/8bcdcb96-646d-4008-93bf-294e92469b3d/jobs/1464?invite=true#step-107-694
    Signed-off-by: Darshan Sen <raisinten@gmail.com>

--- test/test.c
+++ test/test.c
@@ -10,7 +10,11 @@ int main() {
   if (ptr && size > 0) {
     char* str = (char*)malloc(size + 1);
     memset(str, 0, size + 1);
+#if defined(_WIN32)
+    strncpy_s(str, size + 1, ptr, size);
+#else
     strncpy(str, ptr, size);
+#endif
     printf("%s\n", str);
   } else {
     printf("Hello world\n");
