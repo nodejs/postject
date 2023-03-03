e6f79ac951d0ba02bbc4f4e5e86b7418cde2ab59
Darshan Sen <raisinten@gmail.com>
2023-03-03 11:29:45 +0530

    fix: crash in `postject_find_resource()` on Linux (#77)
    * fix: crash in `postject_find_resource()` on Linux
    
    The program headers base address values returned by `getauxval(AT_PHDR)`
    and `dl_iterate_phdr()` are identical only on
    `g++ (Ubuntu 9.4.0-1ubuntu1~20.04.1) 9.4.0`. However, the values are
    totally different on `clang version 10.0.0-4ubuntu1` and
    `g++ (GCC) 8.5.0 20210514 (Red Hat 8.5.0-16)`.
    Since the `dl_iterate_phdr()` approach seems to work for all the 3
    compilers, I think we should proceed with that.
    
    Fixes: https://github.com/nodejs/postject/issues/70
    Refs: https://github.com/nodejs/postject/issues/76
    Signed-off-by: Darshan Sen <raisinten@gmail.com>
    
    * chore: remove unnecessary if block
    
    Signed-off-by: Darshan Sen <raisinten@gmail.com>
    
    * fix: only iterate the main executable program headers
    
    The resource gets injected in the main executable, so there is no need
    to iterate the other shared libraries that are loaded by the program.
    This also resolves a security concern.
    
    Refs: https://github.com/nodejs/postject/pull/77#pullrequestreview-1321016158
    Signed-off-by: Darshan Sen <raisinten@gmail.com>
    
    * chore: shorten change
    
    Signed-off-by: Darshan Sen <raisinten@gmail.com>
    
    * test: add comment about _GNU_SOURCE
    
    Signed-off-by: Darshan Sen <raisinten@gmail.com>
    
    ---------
    
    Signed-off-by: Darshan Sen <raisinten@gmail.com>

--- test/test.c
+++ test/test.c
@@ -1,3 +1,7 @@
+#define _GNU_SOURCE  // This is needed because postject-api.h uses
+                     // dl_iterate_phdr and dl_phdr_info which are non-standard
+                     // GNU extensions.
+
 #include <stdio.h>
 #include <string.h>
 
